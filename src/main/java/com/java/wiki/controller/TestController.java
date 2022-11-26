68e94c074445b24977206e14ddfe13d81857ef51
OctopusLian <zoctopus@qq.com>
2022-11-27 00:26:55 +0800

    mysql 配置

--- src/main/java/com/java/wiki/controller/TestController.java
+++ src/main/java/com/java/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.java.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 //@Controller  //返回页面
 public class TestController {
 
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     @RequestMapping("/hello")  // 支持所有请求方式
     public String hello() {
-        return "hello world";
+        return "hello world"+testHello;
     }
 }
