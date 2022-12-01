bf6a6639836298edac29723a8a5567ca02929b10
OctopusLian <zoctopus@qq.com>
2022-12-01 21:36:10 +0800

    web 集成图标组件

--- wiki-web/src/main.ts
+++ wiki-web/src/main.ts
@@ -4,6 +4,14 @@ import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+import * as Icons from '@ant-design/icons-vue';
 
 // #app，想到选择器，肯定有个div id="app"
-createApp(App).use(store).use(router).use(Antd).mount('#app');
+const app = createApp(App);
+app.use(store).use(router).use(Antd).mount('#app');
+
+// 使用全局图标
+const icons: any=Icons;
+for (const i in icons) {
+    app.component(i,icons[i]);
+}
